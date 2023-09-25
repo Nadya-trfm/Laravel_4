5b01ed8d596d483f4b9f99f836db0dec9fc8d742
Nadezhda Trofimova <n.trofimova@monodigital.ru>
2023-09-25 09:15:05 +0300

    a

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
+    @once
+        @push('scripts')
+            <script src="../js/app.js"></script>
+            <link rel="stylesheet" href="../css/app.css">
+        @endpush
+    @endonce
 </head>
 <body>
 <div>
